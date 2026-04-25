abfa818e61968b65ef0a9a151971e3a8d59975dc
sarah-shtawi <sarah.sht03@gmail.com>
2026-04-25 18:56:55 +0300

    seed data

---
+++ Ecommerce12.DAL/Utils/RoleSeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce12.DAL.Utils
+{
+    public class RoleSeedData : ISeedData
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleSeedData(RoleManager <IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        //Add Role 
+        public async Task DataSeed()
+        {
+            string[] roles = ["SuperAdmin", "Admin", "User"];
+            if (! await  _roleManager.Roles.AnyAsync()) 
+            {
+                foreach (var role in roles) 
+                {
+                   await  _roleManager.CreateAsync(new IdentityRole(role ));
+                }
+
+            }
+        }
+    }
+}
---
+++ Ecommerce12.DAL/Utils/UserSeedData.cs
@@ -0,0 +1,54 @@
+﻿using Ecommerce12.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce12.DAL.Utils
+{
+    public class UserSeedData : ISeedData
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserSeedData(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task DataSeed()
+        {
+            if (!await _userManager.Users.AnyAsync())
+            { // Add Data
+              var user1 = new ApplicationUser {
+                  UserName = "SarahSHtawi1",
+                  Email ="sarah@gmail.com",
+                  FullName = "sarah jalal shatwi",
+                  EmailConfirmed = true 
+              };
+                var user2 = new ApplicationUser 
+                { UserName = "Doaa.Shtawi12", 
+                    Email = "Doaa@gmail.com",
+                    FullName = "Doaa jalal shatwi",
+                    EmailConfirmed = true }; 
+                var user3 = new ApplicationUser
+                { UserName = "Ahmad.Shtawi123",
+                    Email = "Ahmad@gmail.com",
+                    FullName = "Ahmad jalal shatwi",
+                    EmailConfirmed = true 
+                }; 
+                // Add to Data Base
+                await _userManager.CreateAsync(user1, "Sarah.com@123");
+                await _userManager.CreateAsync(user2, "Doaa.com@123"); 
+                await _userManager.CreateAsync(user3, "Ahmad.com@123"); 
+                // Add Roles to user
+                await _userManager.AddToRoleAsync(user1,"SuperAdmin");
+                await _userManager.AddToRoleAsync(user2, "Admin"); 
+                await _userManager.AddToRoleAsync(user3, "User");
+            
+            }
+        }
+    } 
+}
+
+
+            
