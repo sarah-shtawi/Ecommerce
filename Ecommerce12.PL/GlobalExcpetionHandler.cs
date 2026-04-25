da136ff3c53f7527aceabecbfef903a4863b915f
sarah-shtawi <sarah.sht03@gmail.com>
2026-04-25 19:33:42 +0300

    Error Handling

---
+++ Ecommerce12.PL/GlobalExcpetionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce12.DAL.DTO_s.Response;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Ecommerce12.PL
+{
+    public class GlobalExcpetionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorDetails = new ErrorHandling
+            {
+                StatusCode = StatusCodes.Status500InternalServerError,
+                Message = "server error",
+            };
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync(errorDetails);
+            return true;
+
+        }
+    }
+}
